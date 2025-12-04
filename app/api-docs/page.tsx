bf87ccbbe28c969a696f3634ff8845995121ebb1
UTKRRANTI <devops@utkrranti.com>
2025-12-04 13:40:09 +0530

    Fix Swagger UI props to satisfy TS types

--- app/api-docs/page.tsx
+++ app/api-docs/page.tsx
@@ -6,13 +6,10 @@ import 'swagger-ui-react/swagger-ui.css';
 export default function ApiDocsPage() {
   return (
     <div style={{ height: '100vh', width: '100vw' }}>
-      <SwaggerUI
-        url="/api/swagger"
-        docExpansion="none"
-        defaultModelsExpandDepth={0}
-      />
+      <SwaggerUI url="/api/swagger" />
     </div>
   );
 }
+
 
 
